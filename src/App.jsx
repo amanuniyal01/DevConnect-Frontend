9a391975d05a8da228db032538def981dba0959c
amanuniyal01 <amanuniyal315@gmail.com>
2026-05-03 21:00:21 +0530

    Added nord theme in the whole page from node_modules

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ function App() {
   return (
     <div className="p-10">
       <h1 className="text-3xl font-bold  mb-4">DevConnect Frontend</h1>
+      <button className="btn btn-primary">Test Theme</button>
+      
      
     </div>
   )
