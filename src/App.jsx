cbdbcf9026c94cfd60574cf503c8978e7be3053b
amanuniyal01 <amanuniyal315@gmail.com>
2026-05-03 16:16:12 +0530

    Added Daisyui in our application

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 function App() {
   return (
-    <div className='bg-red-500'>Vite installed Successfully..</div>
+    <div className="p-10">
+      <h1 className="text-3xl font-bold text-primary mb-4">DaisyUI Test</h1>
+      <button className="btn btn-primary">Primary Button</button>
+      <button className="btn btn-secondary ml-2">Secondary Button</button>
+      <button className="btn btn-accent ml-2">Accent Button</button>
+    </div>
   )
 }
 
