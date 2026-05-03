f077f32f4effc4dcf03e4fb952606f92d75d9f64
amanuniyal01 <amanuniyal315@gmail.com>
2026-05-03 16:40:07 +0530

    Added theme from daisyui

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,8 @@ import React from 'react'
 function App() {
   return (
     <div className="p-10">
-      <h1 className="text-3xl font-bold text-primary mb-4">DaisyUI Test</h1>
-      <button className="btn btn-primary">Primary Button</button>
-      <button className="btn btn-secondary ml-2">Secondary Button</button>
-      <button className="btn btn-accent ml-2">Accent Button</button>
+      <h1 className="text-3xl font-bold  mb-4">DevConnect Frontend</h1>
+     
     </div>
   )
 }
