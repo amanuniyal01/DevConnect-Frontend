2d69c5778b730419199750fb4959a503f6ac0839
amanuniyal01 <amanuniyal315@gmail.com>
2026-05-12 15:15:46 +0530

    Added a function in body to get user's profile

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,13 +2,30 @@ import React from 'react'
 import Navbar from './Navbar'
 import { Outlet } from 'react-router'
 import Footer from './Footer'
+import axios from 'axios'
+import { BASE_URL } from '../utils/constants'
+import { useDispatch } from 'react-redux'
+import { addUser } from '../utils/userSlice'
 
 function Body() {
+    const dispatch = useDispatch()
+    const fetchUser = async () => {
+        try {
+            const result = await axios.get(BASE_URL + "/profile/view", {
+                withCredentials: true,
+            })
+            dispatch(addUser(result.data));
+        }
+        catch (err) {
+            console.error(err)
+        }
+
+    }
     return (
         <div className='bg-blue-100'>
             <Navbar />
             <Outlet />
-            <Footer/>
+            <Footer />
         </div>
     )
 }
