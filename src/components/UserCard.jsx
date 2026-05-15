af8761236c164627fb6d2a6e9b99b557d36e2f6d
amanuniyal01 <amanuniyal315@gmail.com>
2026-05-15 18:35:26 +0530

    Added info about user in userCard

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { useSelector } from 'react-redux';
 
-function UserCard({user}) {
+function UserCard({ user }) {
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
     const [gender, setGender] = useState("")
@@ -9,17 +9,18 @@ function UserCard({user}) {
     const [photoUrl, setPhotoUrl] = useState("")
     const [skills, setSkills] = useState("");
     const [age, setAge] = useState("")
-  
+
     return (
-        <div>
-           
-            {user?.photoUrl?<>
-            <img src={user?.photoUrl} alt="user photo"/>
-            </>:<>
-            <img src="https://png.pngtree.com/png-vector/20231019/ourmid/pngtree-user-profile-avatar-png-image_10211467.png" alt="user photo"/>
+        <div className='flex items-center justify-center flex-col bg-blue-200 p-6 rounded-4xl shadow-black'>
+
+            {user?.photoUrl ? <>
+                <img className='rounded-lg p-4' src={user?.photoUrl} alt="user photo" />
+            </> : <>
+                <img src="https://png.pngtree.com/png-vector/20231019/ourmid/pngtree-user-profile-avatar-png-image_10211467.png" alt="user photo" />
             </>}
-            <h1>{user.firstName} {user.lastName} </h1>
-            <p>{user.about}</p>
+            <h1 className='font-bold'>{user.firstName} {user.lastName} - {user.gender}</h1>
+        
+            <p className='font-semibold'>{user.about}</p>
             <p>{user.skills}</p>
             <p></p>
         </div>
